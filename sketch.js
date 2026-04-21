7006e922cde8fa3a622c40b2ba2dac51cebef469
timtruong2009-blip <tim.truong.2009@gmail.com>
2026-04-21 15:24:56 -0600

    make starting screen to envision what could be in there

--- sketch.js
+++ sketch.js
@@ -4,12 +4,17 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let startScreen;
 
+function preload(){
+  startScreen = loadImage("screen image/Baguette start screen.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background("red");
+  background("white");
+  image(startScreen, 0,0, windowWidth, windowHeight);
 }
