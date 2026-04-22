a02c5564d72eb5ec71bfbb660d0ca432f2bc57b0
timtruong2009-blip <tim.truong.2009@gmail.com>
2026-04-22 15:25:45 -0600

    try to get font for the game

---
+++ mainscreen.js
@@ -0,0 +1,8 @@
+function mainScreen(){
+  image(startScreen, 0,0, windowWidth, windowHeight);
+  textFont(myFont);
+  textSize(100);
+  fill("black");
+  text("Campaign",100,100);
+
+}
--- sketch.js
+++ sketch.js
@@ -5,16 +5,25 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let startScreen;
+let myFont;
+
+let gamePhrase  = "start";
 
 function preload(){
+  myFont = loadFont("screen image/saiba.ttf");
   startScreen = loadImage("screen image/Baguette start screen.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+  noSmooth();
 }
 
 function draw() {
   background("white");
-  image(startScreen, 0,0, windowWidth, windowHeight);
+  if (gamePhrase === "start"){
+    mainScreen();
+  }
+
 }
