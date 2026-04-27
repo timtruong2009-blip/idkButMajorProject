dcd7dcc2632850fb90a4eddb6e65002247560af7
timtruong2009-blip <tim.truong.2009@gmail.com>
2026-04-27 14:25:39 -0600

    pushy

--- sketch.js
+++ sketch.js
@@ -5,27 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBQvMEmYIfl60Rz-TaDhjv2jheR3iq4thA",
-  authDomain: "compsci30.firebaseapp.com",
-  projectId: "compsci30",
-  storageBucket: "compsci30.firebasestorage.app",
-  messagingSenderId: "785998879392",
-  appId: "1:785998879392:web:afdd4ecbb5d9a3d87987c5",
-  measurementId: "G-7RQ0TZM4SB"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBQvMEmYIfl60Rz-TaDhjv2jheR3iq4thA",
+//   authDomain: "compsci30.firebaseapp.com",
+//   projectId: "compsci30",
+//   storageBucket: "compsci30.firebasestorage.app",
+//   messagingSenderId: "785998879392",
+//   appId: "1:785998879392:web:afdd4ecbb5d9a3d87987c5",
+//   measurementId: "G-7RQ0TZM4SB"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 let startScreen;
 let myFont;
